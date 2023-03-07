404076c6384d62eeacffb213e66aa2225536415d
Ersin Akinci <me@ersinakinci.com>
2023-03-07 12:18:05 -0700

    setup tests

--- packages/builder-fetchers/src/lib/builder-fetchers.spec.ts
+++ packages/builder-fetchers/src/lib/builder-fetchers.spec.ts
@@ -1,7 +1,21 @@
-import { builderFetchers } from './builder-fetchers';
+import { getSome } from './builder-fetchers';
+import { server } from '../mocks/server';
+import { buildquickPages } from '../mocks/data';
 
-describe('builderFetchers', () => {
+// Establish API mocking before all tests.
+beforeAll(() => server.listen());
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers());
+
+// Clean up after the tests are finished.
+afterAll(() => server.close());
+
+describe('getSome', () => {
   it('should work', () => {
-    expect(builderFetchers()).toEqual('builder-fetchers');
+    expect(getSome({ model: 'page', apiKey: 'blah' })).resolves.toStrictEqual(
+      buildquickPages
+    );
   });
 });
