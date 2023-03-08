8fdadf2687cc5e345516f57d94e1d1eb24134f26
Ersin Akinci <me@ersinakinci.com>
2023-03-07 17:48:18 -0700

    remove console.log and debugger

--- packages/builder-fetchers/src/mocks/handlers.ts
+++ packages/builder-fetchers/src/mocks/handlers.ts
@@ -3,8 +3,6 @@ import { buildquickPages } from './data';
 
 export const handlers = [
   rest.get('/api/v2/content/page', (req, res, ctx) => {
-    console.log(req);
-    debugger;
     return res(ctx.status(200), ctx.json({ results: buildquickPages }));
   }),
 ];
