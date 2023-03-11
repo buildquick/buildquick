dbc6485143b94aabfc675b57081af2d2b78a4a2a
Ersin Akinci <me@ersinakinci.com>
2023-03-10 20:10:52 -0700

    fix(builder-utils-react): types for BuilderComponent

--- packages/builder-utils-react/src/lib/components/BuilderComponent.spec.tsx
+++ packages/builder-utils-react/src/lib/components/BuilderComponent.spec.tsx
@@ -1,10 +1,14 @@
+import { ContentItem } from '@buildquick/builder-types';
 import { render } from '@testing-library/react';
 
 import BuilderComponent from './BuilderComponent';
 
 describe('BuilderComponent', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<BuilderComponent inlineContent />);
+    const content = {} as unknown as ContentItem;
+    const { baseElement } = render(
+      <BuilderComponent inlineContent content={content} />
+    );
     expect(baseElement).toBeTruthy();
   });
 });
--- packages/builder-utils-react/src/lib/components/BuilderComponent.tsx
+++ packages/builder-utils-react/src/lib/components/BuilderComponent.tsx
@@ -1,29 +1,40 @@
 import { BuilderComponent as BuilderIoBuilderComponent } from '@builder.io/react';
 import { BuilderComponentProps as BuilderIoBuilderComponentProps } from '@builder.io/react/dist/types/src/components/builder-component.component';
-import { ContentItem } from '@buildquick/builder-types';
+import { ContentItem, Block } from '@buildquick/builder-types';
+import { BuilderContent } from './BuilderContent';
 
 type BuilderComponentProps = {
   [P in keyof BuilderIoBuilderComponentProps]: P extends 'content'
     ? ContentItem
     : P extends 'contentLoaded'
     ? (data: unknown, content: ContentItem) => void
+    : P extends 'builderBlock'
+    ? Block
     : BuilderIoBuilderComponentProps[P];
 };
 
 type BuilderComponent = Omit<
   BuilderIoBuilderComponent,
-  'options' | 'content' | 'inlinedContent' | 'content' | 'onContentLoaded'
+  | keyof React.Component<BuilderIoBuilderComponentProps>
+  | 'options'
+  | 'content'
+  | 'inlinedContent'
+  | 'onContentLoaded'
+  | 'contentRef'
 > & {
-  get options(): Omit<
-    BuilderIoBuilderComponent['options'],
-    'contentLoaded' | 'content'
-  > & {
-    contentLoaded?: ((data: unknown, content: ContentItem) => void) | undefined;
-    content?: ContentItem | undefined;
+  get options(): {
+    [O in keyof BuilderIoBuilderComponent['options']]: O extends 'content'
+      ? ContentItem
+      : O extends 'contentLoaded'
+      ? (data: unknown, content: ContentItem) => void
+      : O extends 'builderBlock'
+      ? Block
+      : BuilderIoBuilderComponent['options'][O];
   };
   get inlinedContent(): ContentItem | undefined;
   get content(): ContentItem | undefined;
   onContentLoaded: (data: unknown, content: ContentItem) => void;
+  contentRef: BuilderContent | null;
 } & React.Component<BuilderComponentProps>;
 
 type BuilderComponentConstructor = new (
