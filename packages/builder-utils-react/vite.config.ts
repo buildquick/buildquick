6412589b64c38b8ba0e635e8b064e205fc355052
Ersin Akinci <me@ersinakinci.com>
2023-03-13 14:00:57 -0700

    chore(builder-utils-react): fix Vite config, externalize Builder deps

--- packages/builder-utils-react/vite.config.ts
+++ packages/builder-utils-react/vite.config.ts
@@ -43,7 +43,13 @@ export default defineConfig({
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      external: [
+        'react',
+        'react-dom',
+        'react/jsx-runtime',
+        '@builder.io/react',
+        '@builder.io/sdk',
+      ],
     },
   },
 
