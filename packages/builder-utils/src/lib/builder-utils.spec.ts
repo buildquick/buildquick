aaffb5409f8b4395eafaf531c359075f0dc5389e
Ersin Akinci <me@ersinakinci.com>
2023-05-18 13:08:03 -0700

    chore(builder-utils): remove unused test

--- packages/builder-utils/src/lib/builder-utils.spec.ts
+++
@@ -1,7 +0,0 @@
-// import { builderUtils } from './builder-utils';
-
-// describe('builderUtils', () => {
-//   it('should work', () => {
-//     expect(builderUtils()).toEqual('builder-utils');
-//   });
-// });
