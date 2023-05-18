2c0d75441d783054483287fc59215089bda86403
Ersin Akinci <me@ersinakinci.com>
2023-05-18 13:07:40 -0700

    feat(builder-utils): add ability to filter handlebars values

---
+++ packages/builder-utils/src/lib/parse-handlebars.spec.ts
@@ -0,0 +1,23 @@
+import { parseHandlebars } from './parse-handlebars';
+
+const data = {
+  world: 'Earth',
+  bar: 'hi',
+};
+
+const filters = {
+  toLowerCase: (val: unknown) => val?.toString()?.toLowerCase() || val,
+};
+
+describe('parseHandlebars', () => {
+  it('should work', () => {
+    expect(parseHandlebars('Hello, {{world}}', data)).toStrictEqual(
+      'Hello, Earth'
+    );
+  });
+  it('should work with filters', () => {
+    expect(
+      parseHandlebars('Hello, {{world | toLowerCase}}', data, filters)
+    ).toStrictEqual('Hello, earth');
+  });
+});
--- packages/builder-utils/src/lib/parse-handlebars.ts
+++ packages/builder-utils/src/lib/parse-handlebars.ts
@@ -1,13 +1,47 @@
 import { get } from './get';
 
+type Filter = (value: unknown) => unknown;
+
+const isFilter = (filter: unknown): filter is Filter =>
+  typeof filter === 'function';
+
+const filter = (
+  filters: Record<string, unknown>,
+  value: unknown,
+  valueFilters: string[],
+  match: string
+): unknown => {
+  if (!valueFilters.length) return value;
+
+  const [currentValueFilter, ...remainingValueFilters] = valueFilters;
+  const currentFilter = filters[currentValueFilter];
+
+  if (!isFilter(currentFilter))
+    throw `Missing or invalid filter provided in template while processing {{${match}}}: ${currentValueFilter}`;
+
+  let newValue;
+
+  try {
+    newValue = currentFilter(value);
+  } catch (e) {
+    console.error(
+      `Error while processing {{${match}}} in template. This filter failed: ${currentFilter}`
+    );
+    throw e;
+  }
+
+  return filter(filters, newValue, remainingValueFilters, match);
+};
+
 export const parseHandlebars = (
   template: string | (() => string),
-  data: Record<string, string>
+  data: Record<string, string>,
+  filters: Record<string, Filter> = {}
 ) => {
   // Check if the template is a string or a function
   template = typeof template === 'function' ? template() : template;
   if (['string', 'number'].indexOf(typeof template) === -1)
-    throw 'PlaceholdersJS: please provide a valid template';
+    throw 'Please provide a valid template';
 
   // If no data, return template as-is
   if (!data) return template;
@@ -17,12 +51,18 @@ export const parseHandlebars = (
     // Remove the wrapping curly braces
     match = match.slice(2, -2);
 
-    // Get the value
-    const val = get(data, match.trim());
+    // Split match into object value path and filters list
+    const [path, ...valueFilters] = match
+      .split('|')
+      .map((val) => val.trim())
+      .filter((val) => val?.length > 0);
+
+    // Get and filter the value
+    const val = filter(filters, get(data, path), valueFilters, match);
 
     // Replace
     if (!val) return '{{' + match + '}}';
-    return val;
+    return val.toString();
   });
 
   return template;
